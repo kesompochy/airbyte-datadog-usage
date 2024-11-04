606521bdbd0593c395ffb8a1e01cdc6f912145ed
kesompochy <95553894+kesompochy@users.noreply.github.com>
2024-11-04 22:40:46 +0900

    fix test

--- unit_tests/test_source.py
+++ unit_tests/test_source.py
@@ -37,8 +37,10 @@ def test_streams(mocker):
         "api_key": "test_api_key",
         "application_key": "test_application_key",
         "site": "datadoghq.com",
-        "product_families": ["all"],
-        "start_date": "2024-01-01T00",
+        "hourly_usage_by_product": {
+            "product_families": ["all"],
+            "start_date": "2024-01-01T00",
+        },
     }
     streams = source.streams(config)
     expected_streams_number = 1
